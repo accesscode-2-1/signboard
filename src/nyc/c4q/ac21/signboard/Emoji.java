7c85f1a7a02979215ac8afd30b2d7f061c17fdf0
sufeiiz <szhao2@gmail.com>
2015-03-28 13:32:30 -0400

    saved strings to variable

--- src/nyc/c4q/ac21/signboard/Emoji.java
+++ src/nyc/c4q/ac21/signboard/Emoji.java
@@ -7,7 +7,23 @@ import java.util.Random;
  */
 public class Emoji {
 
-    // enter emoji. length of string = 20
+    static String left1 = "     ,- *^^* -,     ";
+    static String left2 = "    /          \\    ";
+    static String left3 = "   /    /\\  /\\  \\   ";
+    static String left4 = "  |  \\    ___   \\'  ";
+    static String left5 = "   \\  \\         /\\  ";
+    static String left6 = "    \\          /    ";
+    static String left7 = "     '--v   v-'     ";
+
+    static String right1 = "     ,- *^^* -,     ";
+    static String right2 = "    /          \\    ";
+    static String right3 = "   /  /\\  /\\    \\   ";
+    static String right4 = "  '/   ___   /   |  ";
+    static String right5 = "  /\\        /   /   ";
+    static String right6 = "    \\          /    ";
+    static String right7 = "     '-v   v--'     ";
+
+    // enter emoji
     public static void emoji(SignBoard board) {
         Random random = new Random();
         int width = board.getWidth();
@@ -16,23 +32,23 @@ public class Emoji {
         for (int i = 0; i <= 20; ++i) {
             SignBoard.Frame frame = board.newFrame();
             int jump = random.nextInt(2);
+            int length = 20;
 
-            frame.write(0, y - 3 - jump, "     ,- *^^* -,     ".substring(20 - i));
-            frame.write(0, y - 2 - jump, "    /          \\    ".substring(20 - i));
-            frame.write(0, y - 1 - jump, "   /    /\\  /\\  \\   ".substring(20 - i));
-            frame.write(0, y - jump, "  |  \\    ___   \\'  ".substring(20 - i));
-            frame.write(0, y + 1 - jump, "   \\  \\         /\\  ".substring(20 - i));
-            frame.write(0, y + 2 - jump, "    \\          /    ".substring(20 - i));
-            frame.write(0, y + 3 - jump, "     '--v   v-'     ".substring(20 - i));
+            frame.write(0, y - 3 - jump, left1.substring(length - i));
+            frame.write(0, y - 2 - jump, left2.substring(length - i));
+            frame.write(0, y - 1 - jump, left3.substring(length - i));
+            frame.write(0, y - jump, left4.substring(length - i));
+            frame.write(0, y + 1 - jump, left5.substring(length - i));
+            frame.write(0, y + 2 - jump, left6.substring(length - i));
+            frame.write(0, y + 3 - jump, left7.substring(length - i));
 
-
-            frame.write(width - i, y - 3 - jump, "     ,- *^^* -,     ".substring(0, i));
-            frame.write(width - i, y - 2 - jump, "    /          \\    ".substring(0, i));
-            frame.write(width - i, y - 1 - jump, "   /  /\\  /\\    \\   ".substring(0, i));
-            frame.write(width - i, y - jump, "  '/   ___   /   |  ".substring(0, i));
-            frame.write(width - i, y + 1 - jump, "  /\\        /   /   ".substring(0, i));
-            frame.write(width - i, y + 2 - jump, "    \\          /    ".substring(0, i));
-            frame.write(width - i, y + 3 - jump, "     '-v   v--'     ".substring(0, i));
+            frame.write(width - i, y - 3 - jump, right1.substring(0, i));
+            frame.write(width - i, y - 2 - jump, right2.substring(0, i));
+            frame.write(width - i, y - 1 - jump, right3.substring(0, i));
+            frame.write(width - i, y - jump, right4.substring(0, i));
+            frame.write(width - i, y + 1 - jump, right5.substring(0, i));
+            frame.write(width - i, y + 2 - jump, right6.substring(0, i));
+            frame.write(width - i, y + 3 - jump, right7.substring(0, i));
 
             frame.finish(0.1);
         }
@@ -44,6 +60,7 @@ public class Emoji {
         int width = board.getWidth();
         int y = board.getHeight() / 2;
         int waffley = 4, offsety = 0;
+        int length = 20;
 
         for (int i = 0; i <= numCycles; ++i) {
             SignBoard.Frame frame = board.newFrame();
@@ -58,37 +75,37 @@ public class Emoji {
                 frame.write(1, y + 2 - jump, " *  \\          / *  ");
                 frame.write(1, y + 3 - jump, "*    '--v   v-'     ");
             } else {
-                frame.write(1, y - 3 - jump, "     ,- *^^* -,     ");
-                frame.write(1, y - 2 - jump, "    /          \\    ");
-                frame.write(1, y - 1 - jump, "   /    /\\  /\\  \\   ");
-                frame.write(1, y - jump, "  |  \\    ___   \\'  ");
-                frame.write(1, y + 1 - jump, "   \\  \\         /\\  ");
-                frame.write(1, y + 2 - jump, "    \\          /    ");
-                frame.write(1, y + 3 - jump, "     '--v   v-'     ");
+                frame.write(1, y - 3 - jump, left1);
+                frame.write(1, y - 2 - jump, left2);
+                frame.write(1, y - 1 - jump, left3);
+                frame.write(1, y - jump, left4);
+                frame.write(1, y + 1 - jump, left5);
+                frame.write(1, y + 2 - jump, left6);
+                frame.write(1, y + 3 - jump, left7);
             }
 
             if (jump == 1) {
-                frame.write(width - 20, y - 3 - jump, " *  *,- *^^* -,  *  ");
-                frame.write(width - 20, y - 2 - jump, " *  /          \\   *");
-                frame.write(width - 20, y - 1 - jump, "   /  /\\  /\\    \\   ");
-                frame.write(width - 20, y - jump, "  '/   ___    /  | *");
-                frame.write(width - 20, y + 1 - jump, " _/\\   \\_/  _/  /   ");
-                frame.write(width - 20, y + 2 - jump, "    \\          /    ");
-                frame.write(width - 20, y + 3 - jump, "*    '-v   v--'  *  ");
+                frame.write(width - length, y - 3 - jump, " *  *,- *^^* -,  *  ");
+                frame.write(width - length, y - 2 - jump, " *  /          \\   *");
+                frame.write(width - length, y - 1 - jump, "   /  /\\  /\\    \\   ");
+                frame.write(width - length, y - jump, "  '/   ___    /  | *");
+                frame.write(width - length, y + 1 - jump, " _/\\   \\_/  _/  /   ");
+                frame.write(width - length, y + 2 - jump, "    \\          /    ");
+                frame.write(width - length, y + 3 - jump, "*    '-v   v--'  *  ");
             } else {
-                frame.write(width - 20, y - 3 - jump, "     ,- *^^* -,     ");
-                frame.write(width - 20, y - 2 - jump, "    /          \\    ");
-                frame.write(width - 20, y - 1 - jump, "   /  /\\  /\\    \\   ");
-                frame.write(width - 20, y - jump, "  '/   ___    /  |  ");
-                frame.write(width - 20, y + 1 - jump, "  /\\         /  /   ");
-                frame.write(width - 20, y + 2 - jump, "    \\          /    ");
-                frame.write(width - 20, y + 3 - jump, "     '-v   v--'     ");
+                frame.write(width - length, y - 3 - jump, right1);
+                frame.write(width - length, y - 2 - jump, right2);
+                frame.write(width - length, y - 1 - jump, right3);
+                frame.write(width - length, y - jump, right4);
+                frame.write(width - length, y + 1 - jump, right5);
+                frame.write(width - length, y + 2 - jump, right6);
+                frame.write(width - length, y + 3 - jump, right7);
             }
 
-            frame.write(25, waffley, "   __  ");
-            frame.write(25, waffley + 1, "  /■ ■\\  ");
-            frame.write(25, waffley + 2, " |■ ■ ■|  ");
-            frame.write(25, waffley + 3, "  \\■ ■/  ");
+            frame.write(length + 3, waffley, "   __  ");
+            frame.write(length + 3, waffley + 1, "  /■ ■\\  ");
+            frame.write(length + 3, waffley + 2, " |■ ■ ■|  ");
+            frame.write(length + 3, waffley + 3, "  \\■ ■/  ");
 
             if (waffley < 1) {
                 offsety = 1;
