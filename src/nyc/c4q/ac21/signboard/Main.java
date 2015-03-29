b2f4dddbb39732cf7bee9503dec6932bce2a6d0b
Anthony Fermin <AnthonyFermin@users.noreply.github.com>
2015-03-29 10:16:24 -0400

    Update Main.java

--- src/nyc/c4q/ac21/signboard/Main.java
+++ src/nyc/c4q/ac21/signboard/Main.java
@@ -11,6 +11,144 @@ public class Main {
      *   The number of cycles to draw for.
      */
 
+    public static void superCart (SignBoard board, int numCycles) {
+        int width = board.getWidth();
+        
+
+        String supCart =  "~~~~~~~~~~~~~~~~~~~~~~~ ■■■■■■■■■■■■    ";
+        String supCart1 = "  ~~~~~~~~~~~~~~~~~~~ ■  ■■■  ■      ■  ";
+        String supCart2 = "     ~~~~~~~~~~~~~~ ■    ■    ■ ■■     ■";
+        String supCart3 = "         ~~~~~~~~~~~ ■   ■■■  ■  ■   ■  ";
+        String supCart4 = "               ~~~~~~~ ■           ■    ";
+        String supCart5 = "                  ~~~~~~ ■       ■      ";
+        String supCart6 = "                      ~~~~ ■   ■        ";
+        String supCart7 = "                          ~~ ■          ";
+
+
+
+        for (int x = 0; x < width; ++x) {
+            SignBoard.Frame frame = board.newFrame();
+
+
+
+
+
+            if (x < 0) {
+                // Scrolling on to the left side.
+                setRandColor(frame);
+                frame.write(0, 0, supCart.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 1, supCart1.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 2, supCart2.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 3, supCart3.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 4, supCart4.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 5, supCart5.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 6, supCart6.substring(-x));
+                setRandColor(frame);
+                frame.write(0, 7, supCart7.substring(-x));
+            }
+
+             if (x + supCart.length() <= width) {
+                // Fully on the board.
+                setRandColor(frame);
+                frame.write(x, 0, supCart);
+                setRandColor(frame);
+                frame.write(x, 1, supCart1);
+                setRandColor(frame);
+                frame.write(x, 2, supCart2);
+                setRandColor(frame);
+                frame.write(x, 3, supCart3);
+                setRandColor(frame);
+                frame.write(x, 4, supCart4);
+                setRandColor(frame);
+                frame.write(x, 5, supCart5);
+                setRandColor(frame);
+                frame.write(x, 6, supCart6);
+                setRandColor(frame);
+                frame.write(x, 7, supCart7);
+            }
+            else {
+                // Scrolling off the board.
+                 setRandColor(frame);
+                frame.write(x, 0, supCart.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 1, supCart1.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 2, supCart2.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 3, supCart3.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 4, supCart4.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 5, supCart5.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 6, supCart6.substring(0, width - x));
+                 setRandColor(frame);
+                frame.write(x, 7, supCart7.substring(0, width - x));
+            }
+
+            frame.finish(0.03);
+
+
+        }
+    }
+
+    public static void cheeseDoodle (SignBoard signBoard, int cycles ) {
+
+        int width = signBoard.getWidth();
+        int height = signBoard.getHeight();
+        int x = 0;
+        int y = 0;
+        int xOffset = 1;
+        int yOffset = 1;
+
+
+        String cheeseDoodle =  " ■■■  ■    ■■■ ■■■ ■■■ ■■■ ■■■   ■■■   ■■■■ ■■■■ ■■■   ■   ■■■";
+        String cheeseDoodle1 = " ■    ■ ■■ ■■  ■■  ■■■ ■■■ ■■    ■   ■ ■  ■ ■  ■ ■   ■ ■   ■  ";
+        String cheeseDoodle2 = " ■■■  ■  ■ ■■■ ■■■ ■■■ ■■■ ■■■   ■■■   ■■■■ ■■■■ ■■■   ■■■ ■■■";
+
+
+
+
+        for (int i = 0; i < cycles; ++i) {
+            SignBoard.Frame frame = signBoard.newFrame();
+
+
+            setRandColor(frame);
+            frame.write(x,y,cheeseDoodle);
+            setRandColor(frame);
+            frame.write(x,y+1,cheeseDoodle1);
+            setRandColor(frame);
+            frame.write(x,y+2,cheeseDoodle2);
+
+            y+=yOffset;
+            if(y > 3) {
+                yOffset = -1;
+                y+=yOffset;
+            }
+            if(y < 0) {
+                yOffset = 1;
+                y+=yOffset;
+            }
+            x+=xOffset;
+            if(x > width - cheeseDoodle.length()){
+                xOffset = -1;
+                x+=xOffset;
+            }
+            if(x < 0){
+                xOffset = 1;
+                x+=xOffset;
+            }
+
+
+            frame.finish(0.08);
+        }
+
     public static void ribbonScene(SignBoard board, int numCycles) {
         int width = board.getWidth();
         int height = board.getHeight();
@@ -86,13 +224,6 @@ public class Main {
                 x+=xOffset;
             }
 
-
-
-
-
-
-
-
             frame.finish(0.05);
         }
     }
@@ -123,7 +254,7 @@ public class Main {
                 // Scrolling off the board.
                 frame.write(x, y, text.substring(0, width - x));
 
-            frame.finish(0.02);
+            frame.finish(0.08);
         }
     }
 
@@ -278,13 +409,11 @@ public class Main {
 
         // Run the sign board forever.
         while (true) {
-            ribbonScene(signBoard, 48);
-
+            
+            cheeseDoodle(signBoard,200);
+            superCart(signBoard,100);
             scrollTextScene(signBoard, "~~~~~ ⚡⚡⚡ ★✮★★★★ < SUPER CART > ★★★★★★ ⚡⚡⚡ ~~~~~~");
-            //scrollTextScene(signBoard, "###  SUPER CART  ###");
-            //scrollTextScene(signBoard, "###  SUPER CART  ###");
             foodScene(signBoard, 200);
-            ribbonScene(signBoard, 48);
             dancingSuperCart(signBoard, 18);
             flashFreshHotScene(signBoard, 8);
 
